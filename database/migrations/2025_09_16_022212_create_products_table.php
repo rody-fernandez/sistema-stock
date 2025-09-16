89b0f5a34fcb392e5d67466582908d30ca874660
rody-fernandez <rody.fernandez.saa@gmail.com>
2025-09-15 23:45:34 -0300

    Ajuste de migraciones

--- database/migrations/2025_09_16_022212_create_products_table.php
+++ database/migrations/2025_09_16_022212_create_products_table.php
@@ -4,24 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->text('description')->nullable();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->integer('stock')->default(0);
+            $table->decimal('cost_price', 10, 2);
+            $table->decimal('sale_price', 10, 2);
+            $table->string('image')->nullable(); // ruta de foto
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('products');
     }
 };
--- database/migrations/2025_09_16_022215_create_sales_table.php
+++ database/migrations/2025_09_16_022215_create_sales_table.php
@@ -4,24 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->date('date');
+            $table->decimal('total', 12, 2);
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('sales');
     }
 };
