8d95f4891b6090d8c1c52fffa33e7ff9ca9481ed
rody-fernandez <rody.fernandez.saa@gmail.com>
2025-09-26 19:13:25 -0300

    ajustado app de bootstrap para php artisan serve

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -9,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         channels: __DIR__.'/../routes/channels.php',
     )
-    ->withProviders([
-        App\Providers\FortifyServiceProvider::class,
-    ])
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
     ->create();
